d27f4b355d35720778ff4a7e135f90996258a7df
Chris <krzysztof.daniel@gmail.com>
2021-09-30 21:19:54 +0200

    Revamp the index page. Add nice selection and images.

--- pages/index.js
+++ pages/index.js
@@ -1,92 +1,129 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CardGroup from "react-bootstrap/CardGroup";
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 export default function Home() {
 
-  return (
-    <div className="container">
-      <Head>
-        <title>Wardley Mapping Learning Aid</title>
-      </Head>
+    return (
+        <div className="container">
+            <Head>
+                <title>Wardley Mapping Learning Aid</title>
+            </Head>
 
-      <main>
-        <h1 className="title">
-          What is that you want to practice?
-        </h1>
-          <h1 className="title">
-              <ul>
-                  <li><Link href="/strategycycle">Strategy Cycle</Link></li>
-                  <li><Link href="/componentstostages">Evolution Stage Names</Link></li>
-              </ul>
-          </h1>
-      </main>
+            <main>
 
-      <footer>
-        More things will come one day!
-      </footer>
+                <h1 className="title">
+                    What is that you want to practice?
 
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
+                </h1>
+                <br/>
+                <br/>
+                <CardGroup>
 
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
+                    <Card style={{width: '18rem'}}>
+                        <Card.Img variant="top" src="/strategycycle.jpg"/>
+                        <Card.Body>
+                            <Card.Title>Strategy Cycle</Card.Title>
+                            <Card.Text>
+                                How well do you know Sun Tzu's Five Factors, the OODA loop and the two whys?
+                            </Card.Text>
+                            <Card.Footer>
+                                <Button variant="primary" href="/strategycycle">Start</Button>
+                            </Card.Footer>
 
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
+                        </Card.Body>
+                    </Card>
+                    <Card style={{width: '18rem'}}>
+                        <Card.Img variant="top" src="/cheatsheet.jpg"/>
+                        <Card.Body>
+                            <Card.Title>Component Stage Names</Card.Title>
+                            <Card.Text>
+                                Do you know what is Stage VI of component type 'Capability'? Do you know all the names?
+                            </Card.Text>
+                            <Card.Footer>
+                                <Button variant="primary" href="/componentstostages">Start</Button>
+                            </Card.Footer>
+                        </Card.Body>
+                    </Card>
+                </CardGroup>
+            </main>
+
+            <footer>
+                More things will come one day!
+            </footer>
+
+            <style jsx>{`
+              .container {
+                min-height: 100vh;
+                padding: 0 0.5rem;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+              }
+
+              main {
+                padding: 5rem 0;
+                flex: 1;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+              }
+
+              footer {
+                width: 100%;
+                height: 100px;
+                border-top: 1px solid #eaeaea;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+              }
 
 
+              .title a {
+                color: #0070f3;
+                text-decoration: none;
+              }
 
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
+
+              .title {
+                margin: 0;
+                line-height: 1.15;
+                fontSize: 4rem;
+              }
+
+              .title {
+                text-align: center;
+              }
+              
+              .card-img {
+                width: 100%;
+                height: 15vw;
+                object-fit: cover;
+              }
+              
 
 
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          fontSize: 4rem;
-        }
+            `}</style>
 
-        .title {
-          text-align: center;
-        }
-        
-      `}</style>
+            <style jsx global>{`
+              html,
+              body {
+                padding: 0;
+                margin: 0;
+                font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+                Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+                sans-serif;
+              }
 
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
+              * {
+                box-sizing: border-box;
+              }
+            `}</style>
+        </div>
   )
 }
